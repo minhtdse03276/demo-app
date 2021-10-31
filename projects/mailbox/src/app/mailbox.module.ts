a01354ccd9de89b907083d3b92a7d2e70d8a53cf
anhnt28995 <43347891+anhnt2899@users.noreply.github.com>
2021-11-01 00:19:22 +0700

    update-demo-app

--- projects/mailbox/src/app/mailbox.module.ts
+++ projects/mailbox/src/app/mailbox.module.ts
@@ -1,18 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { CounterComponent } from './counter/counter.component';
 import { MAILBOX_ROUTES } from './app.routes';
 import { MailboxHomeComponent } from './mailbox-home/mailbox-home.component';
-
+import { reducer } from 'src/app/counter/counter.reducers';
+import { StoreModule } from '@ngrx/store';
+import { FormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    MailboxHomeComponent,
-  ],
+  declarations: [MailboxHomeComponent, CounterComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(MAILBOX_ROUTES),
+    StoreModule.forFeature('count', reducer),
+    FormsModule,
   ],
   providers: [],
 })
-export class MailboxModule { }
+export class MailboxModule {}
